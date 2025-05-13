04a803ab19d8df52721eaefebd2f769a90528397
Leonardo Pulpor <pulppor@gmail.com>
2025-05-13 08:58:09 -0300

    feat: add dotenv dependency and update SportsProvider to handle API key securely

--- src/contexts/SportsContext.tsx
+++ src/contexts/SportsContext.tsx
@@ -2,45 +2,52 @@ import { createContext, useState, useEffect } from "react"
 import type { ReactNode } from 'react'
 
 interface SportsContextProps {
-    sports: string[]
-    loading: boolean
+  sports: string[]
+  loading: boolean
 }
 
 export const SportsContext = createContext<SportsContextProps>({
-    sports: [],
-    loading: false,
+  sports: [],
+  loading: false,
 })
 
 export function SportsProvider({ children }: { children: ReactNode }) {
-    const [sports, setSports] = useState<string[]>([])
-    const [loading, setLoading] = useState(false)
+  const [sports, setSports] = useState<string[]>([])
+  const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        async function fetchSports() {
-          try {
-            setLoading(true);
-            const res = await fetch(
-              `https://api.the-odds-api.com/v4/sports?apiKey={${import.meta.env.CHAVE}}`
-            );
-            const data = await res.json();
-            console.log(data); 
-            setSports(data.map((s: any) => s.title));
-          } catch (error) {
-            console.error("Erro ao buscar esportes:", error);
-          } finally {
-            setLoading(false);
-          }
-        }
-
-        fetchSports();
-      }, []);
+  useEffect(() => {
+    async function fetchSports() {
+      const apiKey = import.meta.env.VITE_CHAVE;
       
+      if (!apiKey) {
+        console.error("A chave da API não foi definida.");
+        return;
+      }
+      
+      try {
+        setLoading(true);
+        const res = await fetch(`https://api.the-odds-api.com/v4/sports?apiKey=${apiKey}`);
+        const data = await res.json();
 
-    return (
-        <SportsContext.Provider value={{ sports, loading }}>
-            {children}
-        </SportsContext.Provider>
-    )
+        console.log(data);
+        setSports(data);
+
+      } catch (error) {
+        console.error("Erro ao buscar esportes:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchSports();
+  }, []);
+
+
+  return (
+    <SportsContext.Provider value={{ sports, loading }}>
+      {children}
+    </SportsContext.Provider>
+  )
 }
 
 export default SportsProvider;
